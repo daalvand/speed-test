070ea3a23facbaabb82d60d6a4169328379eae2d
Mehdi <mdaalvand@gmail.com>
2023-07-25 15:17:45 +0330

    refactor

--- readme.md
+++ readme.md
@@ -11,8 +11,8 @@
 
 ### Apache Bench:
 
-![bar-graph.png](results/image-apache-bench-result.png)
+![bar-graph.png](export/image-apache-bench-result.png)
 
 ### Wrk Result:
 
-![wrk-result.png](results/image-wrk-result.png)
+![wrk-result.png](export/image-wrk-result.png)
--- test.sh
+++ test.sh
@@ -39,11 +39,35 @@ services=(
   yii
 )
 
+mkdir -p export/ab
+mkdir -p export/wrk
+
 for service in "${services[@]}"; do
   echo "AB Test For: ${service}..."
-  docker-compose run --rm ab -k -c $CONCURRENCY -t $TIME_LIMIT "http://${service}/hello-world" >"results/ab-${service}-hw-c-${CONCURRENCY}-t${TIME_LIMIT}s.txt"
+  file=${service}-hw-c-${CONCURRENCY}-t${TIME_LIMIT}s.txt
+  docker-compose run --rm ab -k -c $CONCURRENCY -t $TIME_LIMIT -n 1000000 "http://${service}/hello-world" > "export/ab/${file}"
   echo "WRK Test For: ${service}..."
-  docker-compose run --rm wrk -t4 -c $CONCURRENCY -d${TIME_LIMIT}s "http://${service}/hello-world" >"results/wrk-${service}-hw-c-${CONCURRENCY}-t${TIME_LIMIT}s.txt"
+  docker-compose run --rm wrk -t4 -c $CONCURRENCY -d${TIME_LIMIT}s "http://${service}/hello-world" > "export/wrk/${file}"
 done
 
+
 docker-compose down --remove-orphans
+
+csv_file="export/ab-summary-c-${CONCURRENCY}-t${TIME_LIMIT}s.csv"
+echo "service,rps,total" > $csv_file
+for service in "${services[@]}"; do
+  file=${service}-hw-c-${CONCURRENCY}-t${TIME_LIMIT}s.txt
+  rps=$(grep -o 'Requests per second:[[:space:]]*[0-9.]\+' export/ab/${file} | awk '{print $NF}')
+  total=$(grep -o 'Complete requests:[[:space:]]*[0-9]\+' export/ab/${file} | awk '{print $NF}')
+  echo "${service},${rps},${total}" >> $csv_file
+done
+
+
+csv_file="export/wrk-summary-c-${CONCURRENCY}-t${TIME_LIMIT}s.csv"
+echo "service,rps,total" > $csv_file
+for service in "${services[@]}"; do
+  file=${service}-hw-c-${CONCURRENCY}-t${TIME_LIMIT}s.txt
+  rps=$(grep -o 'Requests/sec:[[:space:]]*[0-9.]\+' export/wrk/${file} | awk '{print $NF}')
+  total=$(grep -o '[0-9]\+ requests' export/wrk/${file} | awk '{print $1}')
+  echo "${service},${rps},${total}" >> $csv_file
+done
